d6464d18da3539a98a3c8fc6d68b92e0ffd24d2b
a512949371 <512949371@qq.com>
2018-11-30 11:57:01 +0800

    上线前版本

--- app.js
+++ app.js
@@ -1,52 +1,33 @@
 //app.js
 import Request from './datajson/request.js';
-import httpCall from './datajson/httpCall.js';
-
 App({
   onLaunch: function (options) {
-    // 展示本地存储能力
-    Request.Login(function(res){
-      console.log("Login",res.data)
-      if(res.data.isOk){
+    //展示本地存储能力
+    var that = this;
+    console.log("options", options)
+    var data = options.query.uid || 0;
+    Request.Login(data, function (res) {
+      if (res.data.errno === 0) {
         wx.setStorage({
           key: "token",
           data: res.data.data.token
         })
-        wx.setStorage({
-          key: "isBindPhone",
-          data: res.data.data.isBindPhone
-        })
-        wx.setStorage({
-          key: "isInformation",
-          data: res.data.data.isInformation
-        })
+        wx.setStorageSync("userType", res.data.data.userInfo.isLogin)
+        wx.setStorageSync("mobilePhone", res.data.data.userInfo.mobilePhone)
+        wx.setStorageSync("accId", res.data.data.userInfo.id)
         wx.getSetting({
           success: function (res) {
             if (res.authSetting['scope.userInfo']) {
-              wx.getUserInfo({
-                success: function (res) {
-                  var nickName = res.userInfo.nickName;
-                  var avatarUrl = res.userInfo.avatarUrl;
-                  wx.setStorageSync('nickName', nickName);
-                  wx.setStorageSync('avatarUrl', avatarUrl);
-                  httpCall.updateWxName(avatarUrl, nickName, function (res) {
-                    console.log('updateWxName', res)
-                  });
-                }
-              })
-              wx.switchTab({
+              wx.redirectTo({
                 url: '/pages/index/index',
               })
             }
           }
-        }) 
+        })
       }
-    })
+    })  
   },
   onShow:function(options){
-    var that=this;
-  },
-  globalData: {
-    userInfo: null
+    
   },
 })
